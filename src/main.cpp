916c54c860a5f05e5f69ff27f023363c8991f4fd
Vikrom Narula <vikrom.nar@gmail.com>
2026-09-15 11:15:03 +0700

    refactor: convert from sqlite3 to SQLiteCpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,56 +1,43 @@
 #include <CLI/CLI.hpp>
+#include <SQLiteCpp/SQLiteCpp.h>
 #include <iostream>
-#include <sqlite3.h>
 #include <string>
-
-int callback(void *, int argc, char **argv, char **columnNames) {
-  for (int i = 0; i < argc; ++i) {
-    std::cout << columnNames[i] << " = " << (argv[i] ? argv[i] : "NULL")
-              << '\n';
-  }
-
-  std::cout << '\n';
-  return 0;
-}
 
 int main(int argc, char **argv) {
   CLI::App app;
 
   std::string database_path;
-  app.add_flag("--records", database_path,
-               "This is the path to save the financial records");
+  app.add_option("--records", database_path,
+                 "This is the path to save the financial records");
   CLI11_PARSE(app, argc, argv);
-
-  sqlite3 *database;
-  int connection_status;
 
   if (database_path.empty()) {
     // Create a sqlite file in current directory
-    connection_status = sqlite3_open("financial_records.db", &database);
-  } else {
-    // Connect to sqlite file path
-    connection_status = sqlite3_open(database_path.c_str(), &database);
+    database_path = "financial_records.db";
   }
 
-  if (connection_status != SQLITE_OK) {
-    std::cerr << "Can not connect to the financial records\n";
-    std::cerr << sqlite3_errmsg(database) << '\n';
-    sqlite3_close(database);
+  try {
+    SQLite::Database db(database_path,
+                        SQLite::OPEN_READWRITE | SQLite::OPEN_CREATE);
+
+    SQLite::Statement query(db, "SELECT * FROM OutDummy;");
+
+    while (query.executeStep()) {
+      const int num_columns = query.getColumnCount();
+
+      for (int i = 0; i < num_columns; ++i) {
+        std::cout << query.getColumnName(i) << " = "
+                  << (query.isColumnNull(i) ? "NULL"
+                                            : query.getColumn(i).getText())
+                  << '\n';
+      }
+
+      std::cout << '\n';
+    }
+  } catch (const std::exception &exp) {
+    std::cerr << "SQLite exception: " << exp.what() << '\n';
     return 1;
   }
 
-  const char *query = "SELECT * FROM OutDummy;";
-
-  char *errorMessage = nullptr;
-
-  if (sqlite3_exec(database, query, callback, nullptr, &errorMessage) !=
-      SQLITE_OK) {
-    std::cerr << "Can not run the query\n";
-    std::cerr << sqlite3_errmsg(database) << '\n';
-    sqlite3_free(errorMessage);
-  }
-
-  // Close the connection after program closure
-  sqlite3_close(database);
   return 0;
 }
